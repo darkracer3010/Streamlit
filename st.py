3ff9f62e3b7726ecc45a1c07f4f7963babcf5dfa
Nishal <75416251+darkracer3010@users.noreply.github.com>
2022-01-24 12:06:11 +0530

    Add files via upload

---
+++ st.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("First Application")
+st.header("Website")
+st.write("cppsecrets")
+st.subheader("languages")
+st.write("Python")
+st.write("C++")
+st.markdown('Programming is **_really_ cool**.')
